3d753a286b27aa67e833b7d8e4b1cb26413fb1d6
root <root@localhost.localdomain>
2019-05-04 09:36:08 -0700

    define some strutures

--- src/server/server.c
+++ src/server/server.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include "server.h"
 
+int listen_fd;
+
+//listen at certain port
+int init_server() {
+	//create ipv6 socket
+	if((listen_fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("server socket");
+		return -1;
+	}
+
+	struct sockaddr_in6 serv_addr;
+
+	return 0;
+}
 
 int main() {
-	printf("hello world");
+	if(init_server < 0) {
+		printf("server init failed\n");
+		exit(0);
+	};
+	perror("hello world");
 	return 0;
 }
---
+++ src/server/server.h
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define NUM_USERS 100
+//Message type
+#define IP_REQUEST 100
+#define IP_RESPONSE 101
+#define NET_REQUEST 102
+#define NET_RESPONSE 103
+#define KEEPALIVE 104
+
+//Server info
+#define SERVER_PORT 5000
+
+typedef struct {
+	int length;
+	char type;
+	char data[4096];
+} Msg;
+
+typedef struct user_info_table {
+	int fd; //socket descriptor
+	int cnt; //set to 20 when a user connects, decrease by 1 for every second. Sent keepalive packet when is 0
+	int secs; //the last time receives keepalive
+	struct in_addr v4addr;
+	struct in6_addr v6addr;
+	struct user_info_table* next;
+} user_info_table;
